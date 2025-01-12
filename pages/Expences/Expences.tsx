858d4ec9856ac6565fd2202f489c9a24c1ca354e
ashutoshpaliwal26 <ashutoshpaliwal26@gmail.com>
2025-01-12 17:29:21 +0530

    Adding the Expences Page

--- pages/Expences/Expences.tsx
+++ pages/Expences/Expences.tsx
@@ -1,11 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import { StyleSheet, Text } from 'react-native'
-import { View } from 'react-native'
-import { GLOBALS } from '../../styles/Global'
-import Loading from '../../components/Loader/Loader'
+import React, { useEffect, useState } from "react";
+import {
+  KeyboardAvoidingView,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
+import { View } from "react-native";
+import { GLOBALS } from "../../styles/Global";
+import Loading from "../../components/Loader/Loader";
+import PageHeader from "../../components/PageHeader/PageHeader";
+import ExpenceCard from "../../components/Card/ExpenceCard";
+import AddExpence from "../../components/Form/AddExpence";
 
-const Expences:React.FC = () => {
-    const [loading, setLoading] = useState(false);
+interface InterfaceExpenceData {
+  id : string | number,
+  expenceType : string,
+  date : string,
+  pcs : string,
+  eventYaUser : string,
+  time : string,
+}
+
+const Expences: React.FC = () => {
+  const [addExpence, setAddExpence] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [expenceData, setExpenceData] = useState<InterfaceExpenceData[]>([{
+    id : Date.now(),
+    expenceType : "Char",
+    date : "15 Jan 2025",
+    pcs : "- 20",
+    eventYaUser : "Ashutosh",
+    time : "8:55 AM"
+  }]);
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editData, setEditData] = useState<InterfaceExpenceData | null>(null);
+  
 
   useEffect(() => {
     setLoading(true);
@@ -14,29 +42,82 @@ const Expences:React.FC = () => {
     }, 1000);
   }, []);
 
-  if(loading){
-    return(
-        <Loading/>
-    )
+  if (loading) {
+    return <Loading />;
   }
-    
+
+  const showEditForm = () => {
+    setEdit(!edit);
+  }
+
+  const deleteHandler = (idx: number) => {
+    const events = expenceData.filter((event, i) => i !== idx);
+    setExpenceData(events);
+  };
+
+  const cancelHandler = () => {
+    setAddExpence(!addExpence);
+  };
+
+  const addHandleSave = (data: any) => {
+    setExpenceData((prev) => [...prev, data]);
+    setAddExpence(!addExpence);
+  };
+
+
+
   return (
-    <View style={styles.mainExpences}>
-        <Text>Expences</Text>
-    </View>
-  )
-}
+    <>
+      <KeyboardAvoidingView style={{ flex: 1 }}>
+        <View style={styles.mainEvents}>
+          {addExpence && (
+            <AddExpence handleCancel={cancelHandler} handleSave={addHandleSave} saveBtnText="Add" cancelBtnText="Cancel" title="New Expence"/>
+          )}
+          {/* {edit && (
+            <AddExpence handleCancel={showEditForm} handleSave={addHandleSave} saveBtnText="Save" cancelBtnText="Cancel" title="Edit Event"/>
+          )} */}
+          <PageHeader title="Expences" addHandler={cancelHandler} />
+          <ScrollView
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+          >
+            <View style={styles.centerView}>
+            {
+              expenceData.map((expence)=>{
+                return <ExpenceCard key={expence.id} title={expence.expenceType} date={expence.date} pics={expence.pcs}/>
+              })
+            }
+            </View>
+          </ScrollView>
+        </View>
+      </KeyboardAvoidingView>
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
-    mainExpences : {
-        width : "100%",
-        height : "100%",
-        display : "flex",
-        backgroundColor : GLOBALS.primary,
-        alignItems : "center",
-        justifyContent : "center",
-        position : "relative"
-    }
-})
+  mainEvents: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    backgroundColor: GLOBALS.primary,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    position: "relative",
+  },
+  centerView: {
+    paddingHorizontal: 20,
+    width: "100%",
+    height: "auto",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  eventCards: {
+    width: "100%",
+    height: "auto",
+  },
+});
 
-export default Expences
+export default Expences;
